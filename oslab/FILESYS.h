25714a84ff6de2d2c71c68ce86119bb5ef5076b9
BigStarXu <1014915141@qq.com>
2017-07-08 10:21:29 +0800

    阿萨德

--- oslab/FILESYS.h
+++ oslab/FILESYS.h
@@ -45,19 +45,19 @@
 
 struct inode {
 	struct inode *i_forw;//前驱
-	struct inode *i_back;//
-	char i_flag;
-	unsigned int i_ino;
-	unsigned int i_count;
-	unsigned short di_number;
-	unsigned short di_mode;
-	unsigned short di_uid;
-	unsigned short di_gid;
-	unsigned short di_size;
-	unsigned int di_addr[NADDR];
+	struct inode *i_back;//指向后一个节点的指针
+	char i_flag;//标志位
+	unsigned int i_ino;//inode 号
+	unsigned int i_count;//inode 引用次数
+	unsigned short di_number;//对应的目录树
+	unsigned short di_mode;//访问模式
+	unsigned short di_uid;//用户号
+	unsigned short di_gid;//用户组号
+	unsigned short di_size;//其大小
+	unsigned int di_addr[NADDR];//与数据块的地址有关的数组
 };
 
-struct dinode {
+struct dinode {//这个数据结构对应inode里的参数
 	unsigned short di_number;
 	unsigned short di_mode;
 	unsigned short di_uid;
@@ -67,21 +67,21 @@ struct dinode {
 };
 
 struct direct {
-	char d_name[DIRSIZ];
-	unsigned int d_ino;//目录项结构
+	char d_name[DIRSIZ];//目录名大小
+	unsigned int d_ino;//目录对应的inode号
 };
-
+//超级快数据结构
 struct filsys {
-	unsigned short s_isize;
-	unsigned long s_fsize;
-	unsigned int s_nfree;
+	unsigned short s_isize;//对应的inode大小
+	unsigned long s_fsize;//对应超级快大小
+	unsigned int s_nfree;//指向空闲快指针
 	unsigned short s_pfree;
-	unsigned int s_free[NICFREE];
+	unsigned int s_free[NICFREE];//空闲块数组
 	unsigned int s_ninode;
 	unsigned short s_pinode;
 	unsigned int s_inode[NICINOD];
 	unsigned int s_rinode;
-	char s_fmod;
+	char s_fmod;//访问模式
 };
 
 struct pwd {
